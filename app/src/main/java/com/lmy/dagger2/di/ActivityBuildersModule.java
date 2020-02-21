764dc9f512ebb81509b01ec9fca14a236cc395f3
mingyoung <tkfkd1212!>
2020-02-22 02:21:51 +0900

    added custom scopes

--- app/src/main/java/com/lmy/dagger2/di/ActivityBuildersModule.java
+++ app/src/main/java/com/lmy/dagger2/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.lmy.dagger2.di;
 
 import com.lmy.dagger2.di.auth.AuthModule;
+import com.lmy.dagger2.di.auth.AuthScope;
 import com.lmy.dagger2.di.auth.AuthViewModelsModule;
 import com.lmy.dagger2.di.main.MainFragmentBuildersModule;
 import com.lmy.dagger2.di.main.MainModule;
+import com.lmy.dagger2.di.main.MainScope;
 import com.lmy.dagger2.di.main.MainViewModelsModule;
 import com.lmy.dagger2.ui.auth.AuthActivity;
 import com.lmy.dagger2.ui.main.MainActivity;
@@ -14,6 +16,7 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {
                     AuthViewModelsModule.class,
@@ -22,6 +25,7 @@ public abstract class ActivityBuildersModule {
     )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {
                     MainFragmentBuildersModule.class,
